32e650c851d75b00cd300c720e7bcf112a6cb562
YuJia Qian <124646846@qq.com>
2017-10-13 15:44:07 +0800

    test commit

--- test/src/main/java/com/qyj/controller/TestController.java
+++ test/src/main/java/com/qyj/controller/TestController.java
@@ -1,10 +1,16 @@
 package com.qyj.controller;
 
 /**
- * Created by qianyj on 2017/10/13.
+ *
+ * @author qianyj
+ * @date 2017/10/13
  */
 public class TestController {
     public void test() {
+        System.out.println("111");
+    }
+
+    public static void main(String[] args) {
         System.out.println("111");
     }
 }
